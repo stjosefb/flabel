ae6fa1dbe6f1eac8ef349805a59fa4480d0c6509
Josef <st.josef.b@gmail.com>
2020-06-20 13:53:45 +0700

    API endpoint: grow/refine traces

---
+++ freelabel/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+
+from freelabel import views, viewsTrain, viewsFl
+
+urlpatterns = [
+    path('', views.main, name='main'),
+
+    path('register/', views.register, name='register'), 
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    path('initanns/', views.initanns, name='initanns'),
+    path('refine/', views.refine, name='refine'),
+    path('refine2/', views.refine2, name='refine2'),
+    path('refine3/', views.refine3, name='refine3'),
+    path('cmpGT/', views.cmpGT, name='cmpGT'),
+    path('showFinalImg/', views.showFinalImg, name='showFinalImg'),
+    path('writeLog/', views.writeLog, name='writeLog'),
+    
+    path('play/loadlist/', views.loadlist, name='loadlist'),
+
+    path('play/', views.play, name='play'),
+    path('playFlowers/', viewsFl.playFlowers, name='playFlowers'),
+    path('playFlowers/loadBatches/', viewsFl.loadBatches, name='loadFlower'),
+    path('playFlowers/writeFlLog/', viewsFl.writeFlLog, name='writeFlLog'),
+
+    path('train/', viewsTrain.startTrain, name='play'),
+    path('train/loadTraining/', viewsTrain.loadTraining, name='loadlist'),
+
+    path('video/', views.playVideo, name='playVideo'),
+
+    path('annotate/', views.annotate, name='annotate'),
+    path('annotate/loadlist/', views.loadlistAnnot, name='loadlistAnnot'),
+]
---
+++ lib/python3.7/posixpath.py
@@ -0,0 +1,1 @@
+/home/josef/anaconda3/lib/python3.7/posixpath.py
